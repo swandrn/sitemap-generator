cb2607f618bab0b53017401b8ee362f33186cea6
swandrn <159421702+swandrn@users.noreply.github.com>
2024-09-23 23:25:22 +0200

    refactor to playwright from selenium

--- scrape/main.py
+++ scrape/main.py
@@ -2,20 +2,29 @@ import os
 import sys
 sys.path.append(os.getcwd())
 from utilities import paths
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+from playwright.sync_api import sync_playwright
+from playwright.sync_api import BrowserContext
 import json
 
 user_profile = {}
 
-with open(paths.PROFILE_2) as profile:
+with open(paths.PROFILE_1) as profile:
     user_profile = json.load(profile)
 
-options = webdriver.FirefoxOptions()
-options.add_argument(f'--width={user_profile["headers"]["screenSize"]["width"]}')
-options.add_argument(f'--height={user_profile["headers"]["screenSize"]["height"]}')
-options.set_preference('general.useragent.override', user_profile["headers"]["userAgent"])
-driver = webdriver.Firefox(options=options)
-with driver:
-    driver.get('https://www.scrapethissite.com/')
+with sync_playwright() as p:
+    browser: BrowserContext = p.firefox.launch_persistent_context(
+        user_data_dir='',
+        headless=False,
+        screen=user_profile['headers']['screenSize'],
+        user_agent=user_profile['headers']['userAgent'],
+        locale=user_profile['headers']['locale'],
+        accept_downloads=False,
+    )
+    page = browser.new_page()
+    page.goto('https://www.scrapethissite.com/')
+    anchors = page.query_selector_all('a')
+    links = set()
+    for anchor in anchors:
+        link = anchor.get_attribute('href')
+        links.add(link)
+    print(links)
