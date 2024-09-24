ccf467dcf016c8507b935066399580b765625cec
swandrn <159421702+swandrn@users.noreply.github.com>
2024-09-24 10:53:48 +0200

    only collect links pointing to current domain

--- scrape/main.py
+++ scrape/main.py
@@ -5,6 +5,8 @@ from utilities import paths
 from playwright.sync_api import sync_playwright
 from playwright.sync_api import BrowserContext
 import json
+from urllib.parse import urlparse
+import tldextract
 
 user_profile = {}
 
@@ -21,15 +23,25 @@ with sync_playwright() as p:
         accept_downloads=False,
     )
     browser.add_cookies(user_profile['cookies'])
+
     page = browser.new_page()
-    page.goto('https://www.scrapethissite.com/')
+    # homepage_url = 'https://www.scrapethissite.com/'
+    homepage_url = 'https://www.barstoolsports.com/'
+    # Extract domain name without subdomain and suffix
+    domain = tldextract.extract(homepage_url).domain
+    page.goto(homepage_url)
+
     anchors = page.query_selector_all('a')
     links = set()
     for anchor in anchors:
-        link = anchor.evaluate('(a) => a.href')
-        links.add(link)
-    print(links)
-    #add cookies before page close
+        link: str = anchor.evaluate('(a) => a.href')
+        domain_of_link = urlparse(link).hostname
+        if domain_of_link.find(domain) != -1:
+            links.add(link)
+    for link in links:
+        print(link)
+        
+    # Add cookies before page close
     cookies = browser.cookies()
     user_profile['cookies'] = cookies
     with open(paths.PROFILE_1, 'w') as profile:
