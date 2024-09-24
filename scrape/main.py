8db0a2d238abb3362377c0880aea0633a405f90d
swandrn <159421702+swandrn@users.noreply.github.com>
2024-09-24 16:12:19 +0200

    saving progress

--- scrape/main.py
+++ scrape/main.py
@@ -3,10 +3,56 @@ import sys
 sys.path.append(os.getcwd())
 from utilities import paths
 from playwright.sync_api import sync_playwright
-from playwright.sync_api import BrowserContext
+from playwright.sync_api import BrowserContext, Page
 import json
 from urllib.parse import urlparse
 import tldextract
+
+class Link:
+    def __init__(self, name: str, parent: str | None, children: list[str] | None) -> None:
+        self.name = name
+        self.parent = parent
+        self.children = children
+
+    def getname(self) -> str:
+        return self.name
+
+    def getparent(self) -> str | None:
+        return self.parent
+    
+    def getchildren(self) -> list[str] | None:
+        return self.children
+
+def get_all_keys(dictionary: dict):
+    for key, value in dictionary.items():
+        yield key
+        if isinstance(value, dict):
+            yield from get_all_keys(value)
+
+def get_all_links_to_domain(page: Page, domain: str) -> set:
+    links = set()
+    anchors = page.query_selector_all('a')
+    for anchor in anchors:
+        if not anchor.is_visible():
+            continue
+        link: str = anchor.evaluate('(a) => a.href')
+        if not link:
+            continue
+        domain_of_link = urlparse(link).hostname
+        if domain_of_link.find(domain) != -1:
+            links.add(link)
+    return links
+
+def visit_page(page: Page, url: str, domain: str) -> set:
+    try:
+        page.goto(url)
+    except Exception as e:
+        print(f'error going to {url}: {e}')
+    try:
+        links = get_all_links_to_domain(page, domain)
+    except Exception as e:
+        print(f'error gathering links at {url}: {e}')
+    return links
 
 user_profile = {}
 
@@ -25,21 +71,40 @@ with sync_playwright() as p:
     browser.add_cookies(user_profile['cookies'])
 
     page = browser.new_page()
-    # homepage_url = 'https://www.scrapethissite.com/'
-    homepage_url = 'https://www.barstoolsports.com/'
+    homepage_url = 'https://www.scrapethissite.com/'
+    # homepage_url = 'https://www.barstoolsports.com/'
     # Extract domain name without subdomain and suffix
     domain = tldextract.extract(homepage_url).domain
+    # if not domain:
+    #     # Raise some type of 'domain can't be determined' Exception
     page.goto(homepage_url)
 
-    anchors = page.query_selector_all('a')
     links = set()
-    for anchor in anchors:
-        link: str = anchor.evaluate('(a) => a.href')
-        domain_of_link = urlparse(link).hostname
-        if domain_of_link.find(domain) != -1:
-            links.add(link)
-    for link in links:
-        print(link)
+    visited_links = set()
+    sitemap = {}
+
+    links.update(get_all_links_to_domain(page, domain))
+
+    parent_links = links
+
+    # while visited_links != links:
+    for link in sorted(links):
+        if link in list(get_all_keys(sitemap)):
+            continue
+        nested_links = visit_page(page=page, url=link, domain=domain)
+        l = Link(name=link, parent=None, children=nested_links)
+        sitemap[l.getname()] = l.getchildren()
+        if nested_links:
+            if link in nested_links:
+                nested_links.discard(link)
+            sitemap[link] = dict.fromkeys(nested_links, None)
+        visited_links.add(link)
+        
+    links.update(get_all_keys(sitemap))
+
+    print(json.dumps(sitemap, indent=3))
+    print(sorted(links))
+    print(sorted(visited_links))
         
     # Add cookies before page close
     cookies = browser.cookies()
