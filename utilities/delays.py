41e13ac28ac8f55ba32d9cafb2311031a68f2265
swandrn <159421702+swandrn@users.noreply.github.com>
2024-09-25 21:49:02 +0200

    change delay to ms value

--- utilities/delays.py
+++ utilities/delays.py
@@ -1,8 +1,8 @@
 import time
 import random
 
-def clickDelay() -> float:
-    return random.uniform(0.8, 1.2)
+def clickDelay() -> int:
+    return random.randint(80, 120)
 
 def randomFloat(min: float, max: float) -> float:
     return random.uniform(min, max)
