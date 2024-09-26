b9df09fb9ebdd6c6b8c26921430b9bedd741d014
swandrn <159421702+swandrn@users.noreply.github.com>
2024-09-26 23:27:59 +0200

    create engine

--- storage/database.py
+++ storage/database.py
@@ -1,1 +1,7 @@
+import os
+import sys
+sys.path.append(os.getcwd())
 from sqlalchemy import create_engine
+from utilities import env
+
+engine = create_engine(f'postgresql+psycopg2://{env.DB_USER}:{env.DB_PWD}@{env.DB_HOST}/{env.DB_NAME}')
