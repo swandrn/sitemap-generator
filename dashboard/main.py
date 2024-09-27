8b739744f4522335714eaa181a82786f145ef51f
swandrn <159421702+swandrn@users.noreply.github.com>
2024-09-27 23:59:56 +0200

    orchestrate app

--- dashboard/main.py
+++ dashboard/main.py
@@ -1,8 +1,26 @@
+import os
+import sys
+sys.path.append(os.getcwd())
+import pandas as pd
 from scraper import scrape
+from storage import database
+import tldextract
+import time
 
 homepage_url = 'https://www.scrapethissite.com/'
+domain = tldextract.extract(homepage_url).domain
 
 # If homepage_url exists in database and is not older than 24h:
     # Query sitemap from database
 # Else run scraper:
-    # scrape.run_scraper(homepage_url=homepage_url)
+
+sitemap = scrape.run_scraper(homepage_url=homepage_url, domain=domain)
+
+landing_page_info = {
+    'domain_name': [domain],
+    'page_url': [homepage_url],
+    'last_scraped': [int(time.time())]
+}
+
+database.nested_dict_to_df(dictionary=landing_page_info, db_name='sitemap', table_name='landing_pages')
+database.flat_dict_to_sql(dictionary=sitemap, db_name='sitemap', table_name='pages_of_domain')
