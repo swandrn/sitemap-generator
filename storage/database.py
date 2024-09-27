a919ecb24f69a3fd4dff0b7431e52d2a84cbc364
swandrn <159421702+swandrn@users.noreply.github.com>
2024-09-27 23:59:11 +0200

    create function to insert flat dictionaries and nested dictionaries in sqlite database

--- storage/database.py
+++ storage/database.py
@@ -1,7 +1,15 @@
 import os
 import sys
 sys.path.append(os.getcwd())
+import pandas as pd
 from sqlalchemy import create_engine
-from utilities import env
 
-engine = create_engine(f'postgresql+psycopg2://{env.DB_USER}:{env.DB_PWD}@{env.DB_HOST}/{env.DB_NAME}')
+def flat_dict_to_sql(dictionary: dict, db_name: str, table_name: str):
+    engine = create_engine(f'sqlite:///{db_name}.db')
+    df = pd.DataFrame(list(dictionary.items()), columns=['page_url', 'parent_url'])
+    df.to_sql(table_name, con=engine.raw_connection(), if_exists='append')
+
+def nested_dict_to_df(dictionary: dict, db_name: str, table_name: str):
+    engine = create_engine(f'sqlite:///{db_name}.db')
+    df = pd.DataFrame(dictionary)
+    df.to_sql(table_name, con=engine.raw_connection(), if_exists='append')
