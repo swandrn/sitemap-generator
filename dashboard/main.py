41e26f5189893a994e5279051599832ac50dafa5
swandrn <159421702+swandrn@users.noreply.github.com>
2024-09-29 21:57:51 +0200

    serve sitemap from webserver

--- dashboard/main.py
+++ dashboard/main.py
@@ -5,23 +5,30 @@ from scraper import scrape
 from storage import database
 import tldextract
 import time
-import json
+from flask import Flask
+from flask import request
+from urllib.parse import unquote
 
-homepage_url = 'https://www.scrapethissite.com/'
-domain = tldextract.extract(homepage_url).domain
+app = Flask(__name__)
 
-Session = database.create_session('sitemap')
-session = Session()
+@app.get('/generate/sitemap')
+def generate_sitemap():
+    # 'https://www.scrapethissite.com/'
+    url = unquote(request.args.get('url'))
+    domain = tldextract.extract(url).domain
 
-timestamp = database.get_latest_timestamp(session, url=homepage_url)
-current_time = int(time.time())
+    Session = database.create_session('sitemap')
+    session = Session()
 
-if timestamp and current_time - timestamp < 84600: # If url was scraped within the last 24 hours (86400 seconds)
-    # Query sitemap from database
-    sitemap = database.get_sitemap(session=session, homepage_url=homepage_url)
-    print(json.dumps(sitemap, indent=3))
-else:
-    sitemap = scrape.run_scraper(homepage_url=homepage_url, domain=domain)
-    database.insert_landing_page(session=session, homepage_url=homepage_url, domain=domain, last_scraped=int(time.time()))
-    database.insert_pages_of_domain(session=session, sitemap=sitemap)
-    print(json.dumps(sitemap, indent=3))
+    timestamp = database.get_latest_timestamp(session, url=url)
+    current_time = int(time.time())
+
+    if timestamp and current_time - timestamp < 84600: # If url was scraped within the last 24 hours (86400 seconds)
+        sitemap = database.get_sitemap(session=session, homepage_url=url)
+    else:
+        sitemap = scrape.run_scraper(homepage_url=url, domain=domain)
+        database.insert_landing_page(session=session, homepage_url=url, domain=domain, last_scraped=int(time.time()))
+        database.insert_pages_of_domain(session=session, sitemap=sitemap)
+    return sitemap
+
+app.run(host='localhost', port=8080)
