75def4debc1e601b0abd121186f24602106b52e0
swandrn <159421702+swandrn@users.noreply.github.com>
2024-09-29 14:32:04 +0200

    insert new version of sitemap if cached version is older than 24 hours

--- dashboard/main.py
+++ dashboard/main.py
@@ -13,16 +13,15 @@ domain = tldextract.extract(homepage_url).domain
 Session = database.create_session('sitemap')
 session = Session()
 
-timestamp = database.get_timestamp(session, url=homepage_url)
+timestamp = database.get_latest_timestamp(session, url=homepage_url)
 current_time = int(time.time())
 
-if timestamp and current_time - timestamp < 86400: # If url was scraped within the last 24 hours (86400 seconds)
+if timestamp and current_time - timestamp < 84600: # If url was scraped within the last 24 hours (86400 seconds)
     # Query sitemap from database
     sitemap = database.get_sitemap(session=session, homepage_url=homepage_url)
     print(json.dumps(sitemap, indent=3))
 else:
     sitemap = scrape.run_scraper(homepage_url=homepage_url, domain=domain)
-
     database.insert_landing_page(session=session, homepage_url=homepage_url, domain=domain, last_scraped=int(time.time()))
     database.insert_pages_of_domain(session=session, sitemap=sitemap)
     print(json.dumps(sitemap, indent=3))
