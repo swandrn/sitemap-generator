7e3b1e197bd5fd59d508f343d45dff5cbf066bd2
swandrn <159421702+swandrn@users.noreply.github.com>
2024-10-18 22:34:45 +0200

    custom checkbox and styling

--- frontend/src/components/Homepage/Homepage.tsx
+++ frontend/src/components/Homepage/Homepage.tsx
@@ -1,16 +1,19 @@
-function Homepage() {
+import './Homepage.css';
+
+function Homepage(): JSX.Element {
     return (
         <div>
-            <h1>Sitemap Generator</h1>
             <form action="/generate/sitemap" method="get" >
                 <label htmlFor="url-input">Generate sitemap from URL:</label>
-                <input type="text" name="url" id="url-input" placeholder="https://example.com" />
-                <label htmlFor="subdomain-radio">Limit crawling to current subdomain:</label>
-                <input type="radio" name="subdomain_only" id="subdomain-radio" value="true" />
-                <input type="submit" value="Generate Sitemap" />
+                <input type="text" name="url" id="url-input" className="text-input" placeholder="https://example.com" />
+                <label htmlFor="subdomain-checkbox" className="control control--checkbox">Limit crawling to current subdomain:
+                    <input type="checkbox" name="subdomain_only" id="subdomain-checkbox" value="true" />
+                    <div className="control__indicator"></div>
+                </label>
+                <input type="submit" className="button" value="Generate Sitemap" />
             </form>
         </div >
     );
 }
 
-export {Homepage}
+export { Homepage }
