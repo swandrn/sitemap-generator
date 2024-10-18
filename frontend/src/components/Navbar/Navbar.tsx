6ab1e247f530ad87d5a0f026e3850c819495bf22
swandrn <159421702+swandrn@users.noreply.github.com>
2024-10-18 22:33:05 +0200

    add title to navbar

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -4,6 +4,7 @@ function Navbar(): JSX.Element {
     return (
         <nav>
             <ul className="navbar">
+                <li><h1 className="title">Sitemap Generator</h1></li>
                 <li><a href="/">Home</a></li>
             </ul>
         </nav>
